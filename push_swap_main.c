d49086701857845b1c85974bddc8b5170ab0e20f
Saeko Tanaka <stanaka@e1z3r2p5.42.us.org>
2019-08-22 09:30:09 -0700

    0822am

--- push_swap_main.c
+++ push_swap_main.c
@@ -6,7 +6,7 @@
 /*   By: stanaka <stanaka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/21 12:01:38 by stanaka           #+#    #+#             */
-/*   Updated: 2019/08/22 09:22:52 by stanaka          ###   ########.fr       */
+/*   Updated: 2019/08/22 09:29:45 by stanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,46 +67,45 @@ int		check_num_order(t_data *data, int small)
 
 void	move_half_1(t_data *data, t_pivot_data *p_d)
 {
-	
+	int	tmp;		
+
+	tmp = p_d->small;
+	//while (data->a->top)//nooooooo cause stack A always has some nodes
+	while (tmp > -1)
+	{
+		if (p_d->pivot->i >= data->a->top->i)
+		{
+			node_push(data->b, data->a->top->i);
+			node_pop(data->a);
+			tmp--;
+		}
+		else
+			move_ra(data->a, data->b);
+	}	
 }
 
-void	move_half_1(t_data *data, t_pivot_data *p_d)
+void	move_half_2(t_data *data, t_pivot_data *p_d)
 {
-	
+	int	tmp;		
+
+	tmp = p_d->small;
+	while (tmp > 0)
+	{
+		if (p_d->pivot->i > data->a->top->i)
+		{
+			node_push(data->b, data->a->top->i);
+			node_pop(data->a);
+			tmp--;
+		}
+		else
+			move_ra(data->a, data->b);
+	}	
 }
 
 void	move_half(t_data *data, t_pivot_data *p_d) //ret shows we should get the num (>) or (>=)
 {
-	int	tmp;
-
-	tmp = p_d->small;
 	if (p_d->ret == 1)
-	{
-		//while (data->a->top)//nooooooo cause stack A always has some nodes
-		while (tmp > -1)
-		{
-			if (p_d->pivot->i >= data->a->top->i)
-			{
-				node_push(data->b, data->a->top->i);
-				node_pop(data->a);
-				tmp--;
-			}
-			else
-				move_ra(data->a, data->b);
-		}
-	}
+		move_half_1(data, p_d);
 	else if (p_d->ret == 2)
-	{
-		while (tmp > 0)
-		{
-			if (p_d->pivot->i > data->a->top->i)
-			{
-				node_push(data->b, data->a->top->i);
-				node_pop(data->a);
-				tmp--;
-			}
-			else
-				move_ra(data->a, data->b);
-		}
-	}
+		move_half_2(data, p_d);
 }
